ceedbc930809ab1134a4bf15b5a19752c663c9c0
codewithwaseem <waseemit5426@gmail.com>
2025-10-30 20:48:20 +0500

    updated

--- core/settings.py
+++ core/settings.py
@@ -12,11 +12,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
-import dj_database_url  # If using PostgreSQL on Render
-
-# SECURITY WARNING: update this before deploy
-ALLOWED_HOSTS = ['core.onrender.com']
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -56,13 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # add this
 ]
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 ROOT_URLCONF = 'core.urls'
@@ -93,10 +82,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
-
-DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
 
 # Password validation
